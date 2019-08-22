78aef55ffe52f0534ba54b7512e7fadf5e0b33de
A0162011A <e0112235@u.nus.edu>
2019-08-23 03:08:26 +0800

    feat: basic ui for game state and players

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text timer;
+    public int elapsedTicks = 0;
+    public int ticksPerSecond = 50;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        elapsedTicks += 1;
+        float elapsedSeconds = elapsedTicks / ticksPerSecond;
+        timer.text = elapsedSeconds.ToString();
+    }
+}
