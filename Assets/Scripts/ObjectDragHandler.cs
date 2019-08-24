546520e7a6372f4d205b88bc382369d9c240878d
jo-lyn <e0007247@u.nus.edu>
2019-08-25 01:10:22 +0800

    feat: implement dragging

---
+++ Assets/Scripts/ObjectDragHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDragHandler : MonoBehaviour
+{
+    private float zPos;
+
+    void OnMouseDown()
+    {
+        zPos = Camera.main.WorldToScreenPoint(transform.position).z;
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("x: " + transform.position.x + ", z: " + transform.position.z);
+        // Drop on grid based on (x,z) pos
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = zPos;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
---
+++ Assets/Scripts/UIDragHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
+
+		//Debug.Log("x: " + transform.position.x + ", z: " + transform.position.y);
+		// Drop on grid based on (x,y) pos
+        // Have to translate (x,y) pos to (x,z) pos on board
+	}
+}
