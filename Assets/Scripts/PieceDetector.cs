b9e0c9f33ce9db81f5972b340559d32be504722c
jo-lyn <e0007247@u.nus.edu>
2019-08-25 01:11:20 +0800

    feat: detect drop point on board

---
+++ Assets/Scripts/PieceDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log("hit point: " + hit.point);
+
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = hit.point;
+            }
+        }
+
+    }
+}
