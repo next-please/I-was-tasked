b38413579f41d70155f2f9c4c7638e6b2a5cb8ce
jo-lyn <e0007247@u.nus.edu>
2019-08-25 20:51:38 +0800

    feat: identify piece added

--- Assets/Scripts/PieceDetector.cs
+++ Assets/Scripts/PieceDetector.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class PieceDetector : MonoBehaviour
 {
+    private RaycastHit hit;
+    private Ray ray;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+
+    }
+
+    public void addPiece(string name)
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit)) {
-                Debug.Log("hit point: " + hit.point);
+            Debug.Log("hit point: " + hit.point);
 
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = hit.point;
-            }
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = hit.point;
+
+            // TODO: Snap to grid
+
+            Debug.Log("piece added: " + name);
         }
-
     }
 }
--- Assets/Scripts/UIDragHandler.cs
+++ Assets/Scripts/UIDragHandler.cs
@@ -5,17 +5,17 @@ using UnityEngine.EventSystems;
 
 public class UIDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = Input.mousePosition;
+	public PieceDetector pieceDetector;
+
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
 	}
 
 	public void OnEndDrag(PointerEventData eventData)
-    {
-        transform.localPosition = Vector3.zero;
-
-		//Debug.Log("x: " + transform.position.x + ", z: " + transform.position.y);
-		// Drop on grid based on (x,y) pos
-        // Have to translate (x,y) pos to (x,z) pos on board
+	{
+		transform.localPosition = Vector3.zero;
+		pieceDetector.addPiece(tag);
 	}
 }
