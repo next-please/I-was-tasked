59e8d7e5f23eb2cb5cf25ffb8792e1eb7d3e0ad9
jo-lyn <e0007247@u.nus.edu>
2019-08-28 10:28:56 +0800

    chore: try using UnityEvent

--- Assets/Scripts/UIDragHandler.cs
+++ Assets/Scripts/UIDragHandler.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class UIDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-	public PieceDetector pieceDetector;
+    public UnityEvent onDrop;
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
 
-	public void OnDrag(PointerEventData eventData)
-	{
-		transform.position = Input.mousePosition;
-	}
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
 
-	public void OnEndDrag(PointerEventData eventData)
-	{
-		transform.localPosition = Vector3.zero;
-		pieceDetector.addPiece(tag);
-	}
+        onDrop.Invoke();
+    }
 }
