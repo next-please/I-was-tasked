7f1bf6d62f839d0f2ed3f50674b6c2e458671c66
jo-lyn <e0007247@u.nus.edu>
2019-08-28 23:05:51 +0800

    feat: keep track of next empty slot

--- Assets/Scripts/View/UI/Bench.cs
+++ Assets/Scripts/View/UI/Bench.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Bench : MonoBehaviour
 {
     private const int MAX_SLOTS = 8;
-    private List<BenchItem> items;
+
+    // private List<BenchItem> items;
+    private List<bool> isOccupied;
+    private int nextEmptySlotIndex = 0;
+    private int itemCount = 0;
 
     public GameObject BenchItemPrefab;
 
     void Start()
     {
-        items = new List<BenchItem>();
+        isOccupied = new List<bool>();
+        for (int i = 0; i < MAX_SLOTS; i++)
+        {
+            isOccupied.Add(false);
+        }
+
         // testing
         AddItem(new MeleePiece("Lewis the Jesus Koh", 100, 1, true));
         AddItem(new MeleePiece("Jun the Supreme Kai", 100, 2, true));
@@ -21,25 +30,53 @@ public class Bench : MonoBehaviour
 
     public void AddItem(Piece piece)
     {
-        if (items.Count >= MAX_SLOTS)
+        if (itemCount >= MAX_SLOTS)
         {
             return;
         }
 
+        // create item prefab
         GameObject benchItemObj = Instantiate(BenchItemPrefab) as GameObject;
         BenchItem benchItem = benchItemObj.GetComponent<BenchItem>();
         benchItem.SetPiece(piece);
+        benchItem.SetIndex(nextEmptySlotIndex);
 
-        Transform benchSlot = transform.GetChild(items.Count);
-        benchItemObj.transform.SetParent(benchSlot);
+        // set item prefab as child of slot
+        Transform slot = transform.GetChild(nextEmptySlotIndex);
+        benchItemObj.transform.SetParent(slot);
         benchItemObj.transform.localPosition = Vector3.zero;
 
-        items.Add(benchItem);
+        // update model
+        isOccupied[nextEmptySlotIndex] = true;
+        itemCount++;
+        UpdateNextEmptySlotIndex();
     }
 
-    public void RemoveItem(BenchItem item)
+    public void RemoveItem(int index)
     {
+        if (itemCount == 0)
+        {
+            return;
+        }
+
         // TODO: remove from view
-        items.Remove(item);
+
+
+        // update model
+        isOccupied[index] = false;
+        itemCount--;
+        UpdateNextEmptySlotIndex();
+    }
+
+    private void UpdateNextEmptySlotIndex()
+    {
+        for (int i = 0; i < MAX_SLOTS; i++)
+        {
+            if (!isOccupied[i])
+            {
+                nextEmptySlotIndex = i;
+                return;
+            }
+        }
     }
 }
