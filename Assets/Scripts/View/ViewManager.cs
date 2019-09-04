83d8c4e15c2d027a1812a1121aad86af7558bbe3
jo-lyn <e0007247@u.nus.edu>
2019-09-04 17:32:02 +0800

    fix: remove extra races and jobs

--- Assets/Scripts/View/ViewManager.cs
+++ Assets/Scripts/View/ViewManager.cs
@@ -68,47 +68,25 @@ public class ViewManager : MonoBehaviour
     {
         PieceViewTypeMap = new Dictionary<(Enums.Race, Enums.Job), GameObject>()
         {
-            {(Enums.Race.Dwarf, Enums.Job.Druid), DwarfPrefab},
-            {(Enums.Race.Dwarf, Enums.Job.Mage), DwarfPrefab},
-            {(Enums.Race.Dwarf, Enums.Job.Priest), DwarfPrefab},
-            {(Enums.Race.Dwarf, Enums.Job.Ranger), DwarfPrefab},
-            {(Enums.Race.Dwarf, Enums.Job.Rogue), DwarfPrefab},
-            {(Enums.Race.Dwarf, Enums.Job.Warrior), DwarfPrefab},
-
             {(Enums.Race.Elf, Enums.Job.Druid), ElfPrefab},
             {(Enums.Race.Elf, Enums.Job.Mage), ElfPrefab},
             {(Enums.Race.Elf, Enums.Job.Priest), ElfPrefab},
-            {(Enums.Race.Elf, Enums.Job.Ranger), ElfPrefab},
             {(Enums.Race.Elf, Enums.Job.Rogue), ElfPrefab},
-            {(Enums.Race.Elf, Enums.Job.Warrior), ElfPrefab},
 
             {(Enums.Race.Human, Enums.Job.Druid), HumanPrefab},
             {(Enums.Race.Human, Enums.Job.Mage), HumanPrefab},
             {(Enums.Race.Human, Enums.Job.Priest), HumanPrefab},
-            {(Enums.Race.Human, Enums.Job.Ranger), HumanPrefab},
             {(Enums.Race.Human, Enums.Job.Rogue), HumanPrefab},
-            {(Enums.Race.Human, Enums.Job.Warrior), HumanPrefab},
-
-            {(Enums.Race.Naga, Enums.Job.Druid), NagaPrefab},
-            {(Enums.Race.Naga, Enums.Job.Mage), NagaPrefab},
-            {(Enums.Race.Naga, Enums.Job.Priest), NagaPrefab},
-            {(Enums.Race.Naga, Enums.Job.Ranger), NagaPrefab},
-            {(Enums.Race.Naga, Enums.Job.Rogue), NagaPrefab},
-            {(Enums.Race.Naga, Enums.Job.Warrior), NagaPrefab},
 
             {(Enums.Race.Orc, Enums.Job.Druid), OrcPrefab},
             {(Enums.Race.Orc, Enums.Job.Mage), OrcPrefab},
             {(Enums.Race.Orc, Enums.Job.Priest), OrcPrefab},
-            {(Enums.Race.Orc, Enums.Job.Ranger), OrcPrefab},
             {(Enums.Race.Orc, Enums.Job.Rogue), OrcPrefab},
-            {(Enums.Race.Orc, Enums.Job.Warrior), OrcPrefab},
 
             {(Enums.Race.Undead, Enums.Job.Druid), UndeadPrefab},
             {(Enums.Race.Undead, Enums.Job.Mage), UndeadPrefab},
             {(Enums.Race.Undead, Enums.Job.Priest), UndeadPrefab},
-            {(Enums.Race.Undead, Enums.Job.Ranger), UndeadPrefab},
             {(Enums.Race.Undead, Enums.Job.Rogue), UndeadPrefab},
-            {(Enums.Race.Undead, Enums.Job.Warrior), UndeadPrefab},
         };
     }
 
