49f94925a398e86ff7466f30d8076f25362b4f8d
Yeo Junkai <reginleiff@gmail.com>
2019-09-07 15:11:10 +0800

    feat: add piece movement network handling

---
+++ Assets/Scripts/Network/PieceMovementData.cs
@@ -0,0 +1,17 @@
+﻿namespace Com.Nextplease.IWT
+{
+    public class PieceMovementData : Data
+    {
+        public Player player;
+        public Piece piece;
+        public Tile tile;
+
+        public PieceMovementData(Player player, Piece piece, Tile tile)
+        {
+            this.player = player;
+            this.piece = piece;
+            this.tile = tile;
+        }
+
+    }
+}
--- Assets/Scripts/Network/RequestHandler.cs
+++ Assets/Scripts/Network/RequestHandler.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Com.Nextplease.IWT
 {
-    public class RequestHandler {
+    public class RequestHandler : MonoBehaviour {
+        #region Action Types (move out when too large)
+        private const int MOVE_FROM_BENCH_TO_BOARD = 0;
+        #endregion
+
         #region Manager References
 
         private readonly NetworkManager networkManager;
+        public ArrangementManager arrangementManager;
 
         #endregion
 
-        public RequestHandler()
+        RequestHandler()
         {
             this.networkManager = new NetworkManager(this);
         }
@@ -26,6 +29,10 @@ namespace Com.Nextplease.IWT
         {
             switch(req.GetActionType())
             {
+                case MOVE_FROM_BENCH_TO_BOARD:
+                    PieceMovementData data = (PieceMovementData)req.GetData();
+                    arrangementManager.TryMoveBenchToBoard(data.player, data.piece, data.tile);
+                    break;
                 default:
                     Debug.LogErrorFormat("RequestHandler: {0} issued request of invalid action type {1}", req.GetRequester(), req.GetActionType());
                     break;
