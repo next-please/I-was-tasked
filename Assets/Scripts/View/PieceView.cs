e9ea8111c0e1c0a88cd369e494dc563d2a0c90d2
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-09-15 22:14:19 +0800

    fix: piece move position

--- Assets/Scripts/View/PieceView.cs
+++ Assets/Scripts/View/PieceView.cs
@@ -114,7 +114,9 @@ public class PieceView : MonoBehaviour
     {
         if (e.piece == piece)
         {
-            transform.position = ViewManager.CalculateTileWorldPosition(e.tile);
+            Vector3 piecePosition = ViewManager.CalculateTileWorldPosition(e.tile);
+            piecePosition.y = 0.5f;
+            transform.position = piecePosition;
             transform.rotation = Quaternion.identity;
         }
     }
