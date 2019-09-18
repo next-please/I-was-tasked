38dacbcdaf2a913117859743c1afc6c486465811
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-09-18 20:57:38 +0800

    feat: camera moves

---
+++ Assets/Scripts/View/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    int count = 0;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && count < 2)
+        {
+            transform.position += Vector3.right * 20;
+            count++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && count > 0)
+        {
+            transform.position += Vector3.left * 20;
+            count--;
+        }
+    }
+}
