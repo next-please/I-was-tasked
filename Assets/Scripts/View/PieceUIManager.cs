c5257d1f30c4891d0a8bbc94f94f573bf48c810f
Chua Yun Zhi Nicholas <nicholaschuayunzhi@gmail.com>
2019-09-21 14:31:42 +0800

    feat: piece selection gives more information

--- Assets/Scripts/View/PieceUIManager.cs
+++ Assets/Scripts/View/PieceUIManager.cs
@@ -8,6 +8,9 @@ public class PieceUIManager : MonoBehaviour
     public Canvas pieceCanvas;
     public Text nameText;
     public Text attackDamageText;
+    public Text rangeText;
+    public Text atkSpeedText;
+    public Text rarityText;
 
     void Start()
     {
@@ -50,6 +53,9 @@ public class PieceUIManager : MonoBehaviour
     private void SetPieceInfo(Piece piece)
     {
         nameText.text = piece.GetName();
-        attackDamageText.text = piece.GetAttackDamage().ToString();
+        attackDamageText.text = string.Format("Damage: {0}", piece.GetAttackDamage());
+        rangeText.text = string.Format("Range: {0}", piece.GetAttackRange());
+        atkSpeedText.text = string.Format("Atk Speed: {0}", piece.GetAttackSpeed());
+        rarityText.text = string.Format("Rarity: {0}", piece.GetRarity());
     }
 }
