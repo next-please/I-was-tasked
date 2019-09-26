4f1c595436f83cac722161bc8f8a39edba9216b0
Yeo Junkai <reginleiff@gmail.com>
2019-09-26 14:29:09 +0800

    feat: add logic for room creation

---
+++ Assets/Scripts/Network/Lobby/CreateRoomMenu.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    private readonly string CLASS_NAME = "CreateRoom";
+
+    [SerializeField]
+    private Text _roomName;
+
+    [SerializeField]
+    private byte _maxPlayers;
+    public int MaxPlayers => _maxPlayers; 
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        RoomOptions options = new RoomOptions { MaxPlayers = _maxPlayers };
+        PhotonNetwork.CreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.LogFormat("{0}: Room '{1}' created successfully.", CLASS_NAME, _roomName.text);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.LogFormat("{0}: Room '{1}' creation was unsuccessful - {2}", CLASS_NAME, _roomName.text, message);
+    }
+
+}
--- Assets/Scripts/Network/Lobby/RoomListingsMenu.cs
+++ Assets/Scripts/Network/Lobby/RoomListingsMenu.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateRoom : MonoBehaviour
+public class RoomListingsMenu : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
