f37f2a9493a5915f9e209df9896126d126e9b5f0
Chua Yun Zhi Nicholas <nicholaschuayunzhi@gmail.com>
2019-10-02 18:08:33 +0800

    feat: new scene layout with camera

--- Assets/Scripts/View/CameraController.cs
+++ Assets/Scripts/View/CameraController.cs
@@ -4,61 +4,61 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private readonly Vector3 marketPosition = new Vector3(23.4f, 10.7f, -28f);
-
-    private bool isMoving = false;
-    private Vector3 playerPosition;
-    private Vector3 currPosition;
+    public Transform[] CameraTransforms;
+    private Transform playerTransform;
+    private Transform currTransform;
 
     float speed = 1f;
     int count = 0;
 
     void Awake()
     {
-        playerPosition = transform.position;
-        currPosition = playerPosition;
+        playerTransform = CameraTransforms[0];
+        currTransform = transform;
     }
 
     void Update()
     {
-        if (!isMoving)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && count + 1 < 3)
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow) && count < 2)
-            {
-                StartCoroutine(LerpToPosition(transform.position + Vector3.right * 20));
-                count++;
-            }
+            StopAllCoroutines();
+            StartCoroutine(LerpToTransform(CameraTransforms[count + 1]));
+            count++;
+        }
 
-            if (Input.GetKeyDown(KeyCode.LeftArrow) && count > 0)
-            {
-                StartCoroutine(LerpToPosition(transform.position + Vector3.left * 20));
-                count--;
-            }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && count - 1 >= 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(LerpToTransform(CameraTransforms[count - 1]));
+            count--;
+        }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow) && currPosition != marketPosition)
-            {
-                StartCoroutine(LerpToPosition(marketPosition));
-                currPosition = marketPosition;
-            }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && currTransform != CameraTransforms[3])
+        {
+            StopAllCoroutines();
+            StartCoroutine(LerpToTransform(CameraTransforms[3]));
+            currTransform = CameraTransforms[3];
+            count = -1;
+        }
 
-            if (Input.GetKeyUp(KeyCode.UpArrow) && currPosition != playerPosition)
-            {
-                StartCoroutine(LerpToPosition(playerPosition));
-                currPosition = playerPosition;
-            }
+        if (Input.GetKeyUp(KeyCode.UpArrow) && currTransform != playerTransform)
+        {
+            StopAllCoroutines();
+            StartCoroutine(LerpToTransform(playerTransform));
+            currTransform = playerTransform;
+            count = 0;
         }
     }
 
-    IEnumerator LerpToPosition(Vector3 newPosition)
+    IEnumerator LerpToTransform(Transform newTransform)
     {
-        isMoving = true;
         float duration = 1 / speed;
-
+        Transform currentTransform = transform;
         for (float t = 0; t < duration; t += Time.deltaTime)
         {
-            transform.position = Vector3.Lerp(transform.position, newPosition, t / duration);
+            transform.position = Vector3.Slerp(currentTransform.position, newTransform.position, t / duration);
+            transform.rotation = Quaternion.Slerp(currentTransform.rotation, newTransform.rotation, t / duration);
             yield return 0;
         }
-        isMoving = false;
     }
 }
