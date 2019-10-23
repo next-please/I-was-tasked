2aee93d45fcc05b2802a1761e397d685b67e0ca0
jo-lyn <e0007247@u.nus.edu>
2019-10-23 13:01:00 +0800

    feat: add win lose animation

--- Assets/RoundSummaryUIManager.cs
+++ Assets/RoundSummaryUIManager.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+using Com.Nextplease.IWT;
+
 public class RoundSummaryUIManager : MonoBehaviour
 {
     public Canvas roundSummaryCanvas;
-    public Animator animator;
+    public Animator roundSummaryAnimator;
+    public Animator[] playerBadgeAnimators;
+
+    private int playerNum;
+    private bool isLose;
 
     void Awake()
     {
         roundSummaryCanvas.enabled = false;
+        playerNum = (int)RoomManager.GetLocalPlayer();
     }
 
     void OnEnable()
     {
         EventManager.Instance.AddListener<EnterPhaseEvent>(OnEnterPhase);
+        EventManager.Instance.AddListener<DamageTakenEvent>(OnDamageTaken);
     }
 
     void OnDisable()
@@ -29,13 +37,36 @@ public class RoundSummaryUIManager : MonoBehaviour
         }
     }
 
+    void OnDamageTaken(DamageTakenEvent e)
+    {
+        isLose = true;
+    }
+
     IEnumerator ShowRoundSummary()
     {
-        roundSummaryCanvas.enabled = true;
-        animator.Play("Enter");
-        yield return new WaitForSeconds(3f);
-        animator.Play("Exit");
         yield return new WaitForSeconds(1f);
+
+        roundSummaryCanvas.enabled = true;
+        roundSummaryAnimator.Play("Enter");
+
+        // play win/lose
+        yield return new WaitForSeconds(playerNum * 0.3f);
+        if (isLose)
+        {
+            playerBadgeAnimators[playerNum].SetTrigger("Lose");
+        }
+        else
+        {
+            playerBadgeAnimators[playerNum].SetTrigger("Win");
+        }
+        isLose = false;
+
+        yield return new WaitForSeconds(5f);
+        roundSummaryAnimator.Play("Exit");
+        yield return new WaitForSeconds(1f);
+
+        // reset state
         roundSummaryCanvas.enabled = false;
+        playerBadgeAnimators[playerNum].Play("Idle");
     }
 }
--- Assets/Scripts/Logic/Master/PhaseManager.cs
+++ Assets/Scripts/Logic/Master/PhaseManager.cs
@@ -234,7 +234,7 @@ public class PhaseManager : MonoBehaviour
     IEnumerator PostCombatToStartRound()
     {
         ChangePhase(Phase.PostCombat);
-        yield return Countdown(5);
+        yield return Countdown(7);
         TryStartRound();
     }
 
