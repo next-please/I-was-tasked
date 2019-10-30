d67b174108e171368a8a322c905f6fbd2c92e071
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-10-31 01:51:16 +0800

    feat: human synergy aura

---
+++ Assets/Scripts/View/Synergy/HumanSynergyView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class HumanSynergyView : MonoBehaviour
+{
+    public VisualEffect vfx;
+    private PieceView pieceView;
+
+    void Start()
+    {
+        EventManager.Instance.AddListener<RaceSynergyAppliedEvent>(OnSynergyApplied);
+        EventManager.Instance.AddListener<ExitPhaseEvent>(OnExitPhase);
+        pieceView = GetComponentInParent<PieceView>();
+        if (pieceView == null)
+            Destroy(this);
+    }
+
+    void OnDestroy()
+    {
+        EventManager.Instance.RemoveListener<RaceSynergyAppliedEvent>(OnSynergyApplied);
+        EventManager.Instance.RemoveListener<ExitPhaseEvent>(OnExitPhase);
+    }
+
+    void OnSynergyApplied(RaceSynergyAppliedEvent e)
+    {
+         if (e.Race != Enums.Race.Human)
+            return;
+        Piece piece = pieceView.piece;
+        if (piece.IsOnBoard() && !piece.IsEnemy())
+        {
+            vfx.SendEvent("Start");
+        }
+    }
+
+    void OnExitPhase(ExitPhaseEvent e)
+    {
+        if (e.phase == Phase.Combat)
+        {
+            vfx.SendEvent("Stop");
+        }
+    }
+}
