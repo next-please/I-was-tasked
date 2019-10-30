995728e99b94d400f89325c503dd64cbebde985a
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-10-31 01:56:54 +0800

    feat: druid synergy aura

---
+++ Assets/Scripts/View/Synergy/DruidSynergyView.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class DruidSynergyView : MonoBehaviour
+{
+    public VisualEffect vfx;
+
+    private PieceView pieceView;
+
+    void Start()
+    {
+        EventManager.Instance.AddListener<JobSynergyAppliedEvent>(OnSynergyApplied);
+        EventManager.Instance.AddListener<ExitPhaseEvent>(OnExitPhase);
+        pieceView = GetComponentInParent<PieceView>();
+        if (pieceView == null)
+            Destroy(this);
+    }
+
+    void OnDestroy()
+    {
+        EventManager.Instance.RemoveListener<JobSynergyAppliedEvent>(OnSynergyApplied);
+        EventManager.Instance.RemoveListener<ExitPhaseEvent>(OnExitPhase);
+    }
+
+    void OnSynergyApplied(JobSynergyAppliedEvent e)
+    {
+        if (e.Job != Enums.Job.Druid)
+            return;
+        Piece piece = pieceView.piece;
+        if (piece.IsOnBoard() && !piece.IsEnemy())
+        {
+            vfx.SendEvent("Start");
+        }
+    }
+
+    void OnExitPhase(ExitPhaseEvent e)
+    {
+        if (e.phase == Phase.Combat)
+        {
+            vfx.SendEvent("Stop");
+        }
+    }
+}
