04d1277f6dc3626b0c379cd78b2d7d1c19c129dc
jo-lyn <e0007247@u.nus.edu>
2019-10-30 13:20:45 +0800

    feat: move current army size text to board

--- Assets/Scripts/View/UI/InventoryUIManager.cs
+++ Assets/Scripts/View/UI/InventoryUIManager.cs
@@ -7,17 +7,27 @@ using TMPro;
 
 public class InventoryUIManager : MonoBehaviour
 {
+    public Canvas CurrentArmySizeCanvas;
+
+    public TextMeshProUGUI CurrentArmySizeText;
     public TextMeshProUGUI ArmySizeText;
     public TextMeshProUGUI GoldText;
+
+    void Awake()
+    {
+        CurrentArmySizeCanvas.enabled = false;
+    }
 
     void OnEnable()
     {
         EventManager.Instance.AddListener<InventoryChangeEvent>(OnInventoryChange);
+        EventManager.Instance.AddListener<CameraPanEvent>(OnCameraPan);
     }
 
     void OnDisable()
     {
         EventManager.Instance.RemoveListener<InventoryChangeEvent>(OnInventoryChange);
+        EventManager.Instance.RemoveListener<CameraPanEvent>(OnCameraPan);
     }
 
     void OnInventoryChange(InventoryChangeEvent e)
@@ -29,8 +39,20 @@ public class InventoryUIManager : MonoBehaviour
         if (RoomManager.GetLocalPlayer() == owner)
         {
             GoldText.text = inv.GetGold().ToString();
-            // ArmySizeText.text = "Army Size: " + inv.GetArmyCount() + "/" + inv.GetArmySize();
             ArmySizeText.text = inv.GetArmySize().ToString();
+            CurrentArmySizeText.text = inv.GetArmyCount() + "/" + inv.GetArmySize();
+        }
+    }
+
+    void OnCameraPan(CameraPanEvent e)
+    {
+        if (CameraController.IsViewingOwnBoard())
+        {
+            CurrentArmySizeCanvas.enabled = true;
+        }
+        else
+        {
+            CurrentArmySizeCanvas.enabled = false;
         }
     }
 }
