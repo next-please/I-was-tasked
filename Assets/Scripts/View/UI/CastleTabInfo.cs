7ea77e2df2dda89ff9c94ce3c16b708d0e149e99
Rinder5 <Rinder4@hotmail.com>
2019-11-08 15:11:13 +0800

    feat: add mouseover castle health display

---
+++ Assets/Scripts/View/UI/CastleTabInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class CastleTabInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject castleHealth;
+    public TextMeshProUGUI _description;
+    public MarketManager marketManager;
+
+    public void Awake()
+    {
+        castleHealth.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        _description.SetText(marketManager.GetCastleHealth() + " / " + MarketManager.StartingCastleHealth);
+        castleHealth.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        castleHealth.SetActive(false);
+    }
+
+}
