2d4ccd7eb6ca71034e17d83487cb3e1ce0639290
Junkai <reginleiff@gmail.com>
2019-11-09 17:18:18 +0800

    fix: players with identical names cannot play together

--- Assets/Scripts/Network/RoomManager.cs
+++ Assets/Scripts/Network/RoomManager.cs
@@ -91,7 +91,7 @@ namespace Com.Nextplease.IWT
             for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
             {
                 Photon.Realtime.Player player = PhotonNetwork.PlayerList[i];
-                _playerMap.Add(player.NickName, i);
+                _playerMap.Add(player.UserId, i);
                 Debug.LogFormat("{0}: Added {1} to playerMap as index {1}", "RoomManager", player.NickName, i);
             }
             return;
