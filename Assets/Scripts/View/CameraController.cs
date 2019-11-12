9a80c5fa8c05d795630f21e3ce479a019a4b625d
Rinder5 <Rinder4@hotmail.com>
2019-11-12 20:08:57 +0800

    fix: super minor inconseqential bug about player avatar

--- Assets/Scripts/View/CameraController.cs
+++ Assets/Scripts/View/CameraController.cs
@@ -52,16 +52,14 @@ public class CameraController : MonoBehaviour
             playerPosition--;
             EventManager.Instance.Raise(new CameraPanEvent { targetView = (CameraView)playerPosition });
         }
-
-        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && playerPosition != -1)
+        else if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && playerPosition != -1)
         {
             StopAllCoroutines();
             StartCoroutine(LerpToTransform(CameraTransforms[3]));
             playerPosition = -1;
             EventManager.Instance.Raise(new CameraPanEvent { targetView = (CameraView)playerPosition });
         }
-
-        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && playerPosition == -1)
+        else if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && playerPosition == -1)
         {
             StopAllCoroutines();
             StartCoroutine(LerpToTransform(playerTransform));
