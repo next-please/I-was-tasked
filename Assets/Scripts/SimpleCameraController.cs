364ce798c13c58c5ea2dd7106f7dfad72e042f84
Stanley Tay <saintpatricktay@gmail.com>
2019-11-13 07:24:21 +0800

    fix: remove esc key functionality for camera

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -108,15 +108,14 @@ namespace UnityTemplateProjects
         
         void Update()
         {
-            // Exit Sample  
-
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                Application.Quit();
-				#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false; 
-				#endif
-            }
+            // Exit Sample
+            //if (Input.GetKey(KeyCode.Escape))
+            //{
+            //    Application.Quit();
+            //  #if UNITY_EDITOR
+            //  UnityEditor.EditorApplication.isPlaying = false; 
+            //  #endif
+            //}
             // Hide and lock cursor when right mouse button pressed
             if (Input.GetMouseButtonDown(1))
             {
